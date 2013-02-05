3dda2eae54e0268e52b3b4a9d20b06f7edb08566
John Hume <duelin.markers@gmail.com>
2013-02-05 09:52:03 -0600

    Define the same simple macro three ways.

--- src/macro_madness/core.clj
+++ src/macro_madness/core.clj
@@ -1,6 +1,18 @@
 (ns macro-madness.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Simple macros.
+
+(defmacro defmap0
+  "Defines a map, implemented with quote and nothing else."
+  [name & kvs]
+  (list 'def name (cons 'hash-map kvs)))
+
+(defmacro defmap
+  "Defines a map, implemented with syntax-quote and unquote."
+  [name & {:as kvs}]
+  `(def ~name ~kvs))
+
+(defmacro defmap2
+  "Defines a map, implemented with syntax-quote and unquote-splicing."
+  [name & kvs]
+  `(def ~name (hash-map ~@kvs)))
--- test/macro_madness/core_test.clj
+++ test/macro_madness/core_test.clj
@@ -1,7 +1,22 @@
 (ns macro-madness.core-test
-  (:use clojure.test
-        macro-madness.core))
+  (:use clojure.test macro-madness.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def expected-map {:k1 "kay one" :k2 "kay two"})
+
+(defmap0 my-map0 :k1 "kay one" :k2 "kay two")
+(defmap my-map :k1 "kay one" :k2 "kay two")
+(defmap2 my-map2 :k1 "kay one" :k2 "kay two")
+
+(deftest simple-macros
+  (testing "defmap0"
+    (is (= my-map0 expected-map))
+    (is (= (macroexpand '(defmap0 foo :a "Aye"))
+           '(def foo (hash-map :a "Aye")))))
+  (testing "defmap"
+    (is (= my-map expected-map))
+    (is (= (macroexpand '(defmap foo :a "Aye"))
+           '(def foo {:a "Aye"}))))
+  (testing "defmap2"
+    (is (= my-map2 expected-map))
+    (is (= (macroexpand '(defmap2 foo :a "Aye"))
+           '(def foo (clojure.core/hash-map :a "Aye"))))))
