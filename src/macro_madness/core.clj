6a7746b5da75e882967730d020031e15253a562f
John Hume <duelin.markers@gmail.com>
2013-02-24 09:50:55 -0600

    Add lots of stuff.

--- src/macro_madness/core.clj
+++ src/macro_madness/core.clj
@@ -1,4 +1,5 @@
-(ns macro-madness.core)
+(ns macro-madness.core
+  (:require clojure.tools.macro))
 
 ;; Simple macros.
 
@@ -16,3 +17,100 @@
   "Defines a map, implemented with syntax-quote and unquote-splicing."
   [name & kvs]
   `(def ~name (hash-map ~@kvs)))
+
+
+;; Slightly less simple, with some common techniques.
+
+(defmacro defenum [& syms]
+  ;; Wrap a (do ...) around multiple independent expressions.
+  `(do
+     ;; Prefix code that returns a seq of forms with ~@
+     ~@(for [sym syms]
+         `(def ~sym ~(name sym)))
+     ;; Litter the callee's namespace with new vars or capture its
+     ;; vars with ~' -- but only if you're sure you want to.
+     (def ~'values [~@syms])
+     (def ~'length ~(count syms))))
+
+
+;; Common technique: Convert a body into a fn and use an underlying HOF.
+
+(defn try-times*
+  "Executes thunk. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n thunk]
+  (loop [n n]
+    (if-let [result (try
+                      [(thunk)]
+                      (catch Exception e
+                        (when (= 1 n)
+                          (throw e))))]
+      (result 0)
+      (recur (dec n)))))
+
+(defmacro try-times
+  "Executes body. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n & body]
+  `(try-times* ~n (fn [] ~@body)))
+;; Example from Meikel Brandmeyer (kotarak) at http://stackoverflow.com/a/1879961/117989
+
+
+;; What's defmacro really doing?
+(comment
+
+  (macroexpand-1
+   '(defmacro defmap
+      "Defines a map, implemented with syntax-quote and unquote."
+      [name & {:as kvs}]
+      `(def ~name ~kvs)))
+  ;; returns (eliding namespace qualification)
+  (do
+    (defn defmap "Defines a map, implemented with syntax-quote and unquote."
+      ([&form &env name & {:as kvs}]
+         (seq (concat (list (quote def)) (list name) (list kvs)))))
+    (. (var defmap) (setMacro))
+    (var defmap)))
+
+(defmacro dump-macro-goodness
+  "Prints what it knows at compile time. Expands to nothing."
+  [& args]
+  (println "\n=== Macro Goodness ===\n\n"
+           "&form is" (pr-str &form) "with metadata" (meta &form) "\n\n"
+           "&env is" (pr-str &env) "\n\n"
+           (if &env
+             (for [[k v] &env] (str k " " (bean v) "\n\n"))
+             "\n")
+           "args are" (pr-str args)))
+
+(comment
+  (dump-macro-goodness)
+  (dump-macro-goodness 1 "hello")
+  (let [a 3] (dump-macro-goodness))
+  (let [a 3
+        b "abc"
+        ^CharSequence c "def"]
+    (dump-macro-goodness 1 2 a))
+  )
+
+
+;; clojure.tools.macro stuff
+(defmacro defmultimethod
+  "Defines a complete multimethod, combining a defmulti and multiple defmethod calls into one form.
+  Call like:
+    (defmultimethod my-method my-dispatch-fn
+      {:default \"my-weird-default\" :hierarchy my-own-hierarchy}
+      (\"my-weird-default\" [_ _] (println \"we've defaulted!\"))
+      (dispatch-val-1 [x y] (println x y)))"
+  {:arglists '([name doc-string? attr-map? dispatch-fn options-map? & methods])}
+  [name & args]
+  (let [[name [dispatch-fn & args]] (clojure.tools.macro/name-with-attributes name args)
+        [options & methods] (if (map? (first args))
+                              args
+                              (cons {} args))]
+    (apply list 'do
+           (apply list `defmulti name dispatch-fn (flatten (seq options)))
+           (for [method methods]
+             (apply list `defmethod name method)))))
--- test/macro_madness/core_test.clj
+++ test/macro_madness/core_test.clj
@@ -1,22 +1,62 @@
 (ns macro-madness.core-test
   (:use clojure.test macro-madness.core))
 
-(def expected-map {:k1 "kay one" :k2 "kay two"})
+(deftest simple-defmap-macros
 
-(defmap0 my-map0 :k1 "kay one" :k2 "kay two")
-(defmap my-map :k1 "kay one" :k2 "kay two")
-(defmap2 my-map2 :k1 "kay one" :k2 "kay two")
+  (def expected-map {:k1 "one" :k2 "two"})
 
-(deftest simple-macros
   (testing "defmap0"
+    (defmap0 my-map0 :k1 "one" :k2 "two")
     (is (= my-map0 expected-map))
-    (is (= (macroexpand '(defmap0 foo :a "Aye"))
-           '(def foo (hash-map :a "Aye")))))
+    (comment ; due to clojure.test bug
+      (is (= (macroexpand '(defmap0 foo :a "Aye")) '(def foo (hash-map :a "Aye"))))))
+
   (testing "defmap"
+    (defmap my-map :k1 "one" :k2 "two")
     (is (= my-map expected-map))
-    (is (= (macroexpand '(defmap foo :a "Aye"))
-           '(def foo {:a "Aye"}))))
+    (comment
+      (is (= (macroexpand '(defmap foo :a "Aye")) '(def foo {:a "Aye"})))))
+
   (testing "defmap2"
+    (defmap2 my-map2 :k1 "one" :k2 "two")
     (is (= my-map2 expected-map))
-    (is (= (macroexpand '(defmap2 foo :a "Aye"))
-           '(def foo (clojure.core/hash-map :a "Aye"))))))
+    (comment
+      (is (= (macroexpand '(defmap2 foo :a "Aye")) '(def foo (clojure.core/hash-map :a "Aye")))))))
+
+(deftest try-times-macro-and-fn
+
+  (defn inc-and-throw [an-atom]
+    (swap! an-atom inc)
+    (throw (RuntimeException. (str "on attempt " @an-atom))))
+
+  (testing "macro and fn both try the given number of times"
+    (let [attempts (atom 0)
+          caught (try
+                   (try-times 3 (inc-and-throw attempts))
+                   (catch RuntimeException e e))]
+      (is (= 3 @attempts))
+      (is (= "on attempt 3" (.getMessage caught))))
+
+
+    (let [attempts (atom 0)
+          caught (try
+                   (try-times* 3 #(inc-and-throw attempts))
+                   (catch RuntimeException e e))]
+      (is (= 3 @attempts))
+      (is (= "on attempt 3" (.getMessage caught)))))
+
+  (testing "the fn can be composed, while the macro cannot"
+    (let [try-twice (partial try-times* 2)
+          attempts (atom 0)]
+      (try
+        (try-twice #(inc-and-throw attempts))
+        (catch RuntimeException e nil))
+      (is (= 2 @attempts)))
+
+    (comment ; this won't compile
+      (let [try-twice (partial try-times 2) ;; CompilerException "Can't take value of a macro"
+            attempts (atom 0)]
+        (try
+          (try-twice (inc-and-throw attempts))
+          (catch RuntimeException e nil))
+        (is (= 2 @attempts))))))
